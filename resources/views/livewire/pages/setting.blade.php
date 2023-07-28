327368b21c17e15c9e5b4ea919853cfc3e3efc98
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-07-28 20:32:26 +0700

    add

--- resources/views/livewire/pages/setting.blade.php
+++ resources/views/livewire/pages/setting.blade.php
@@ -1,5 +1,4 @@
 <form class="space-y-6" wire:submit.prevent="save">
-
     <div>
         <label for="new-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
         <input wire:model="new_password" type="password" name="new-password" id="new-password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="••••••••">
@@ -16,5 +15,5 @@
         @enderror
     </div>
 
-    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" class="w-full text-white bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
--- resources/views/voter/dashboard.blade.php
+++ resources/views/voter/dashboard.blade.php
@@ -5,19 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>EVoting</title>
-   <!-- Fonts Poppins -->
-   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Fonts Poppins -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-   @livewireStyles
+    <link rel="stylesheet" href="/custom/main.css">
+    @livewireStyles
 
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
 
     <style>
       body {
         font-family: 'Poppins', sans-serif;
       }
     </style>
-  
 </head>
 <body>
 <div class="flex flex-col justify-between h-screen">
